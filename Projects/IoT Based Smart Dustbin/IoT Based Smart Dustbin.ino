a835a027ae15277c536bccac07317b4ce34e8050
Mohammed Aquib Ansari <75412827+aquib12377@users.noreply.github.com>
2023-07-25 00:18:58 +0530

    Update IoT Based Smart Dustbin.ino

--- Projects/IoT Based Smart Dustbin/IoT Based Smart Dustbin.ino
+++ Projects/IoT Based Smart Dustbin/IoT Based Smart Dustbin.ino
@@ -1,42 +1,67 @@
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+/*
+Tutorial — IoT Based Smart Dustbin Monitoring System using Blynk and NodeMcu
+Date Created — 07/02/2020
+Link to the post — https://alphaelectronz.com/smart-dustbin-monitoring-iot/
+*/
 
-  // the loop function runs over and over again forever
-  void loop() {
-    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-    delay(1000); // wait for a second
-    digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
-    delay(1000); // wait for a second
+#define BLYNK_PRINT Serial#include <ESP8266_Lib.h>
+
+#include <BlynkSimpleShieldEsp8266.h>
+
+#include<Servo.h>
+
+char auth[] = “your Auth Token”;
+char ssid[] = “Your Wifi Name”;
+char pass[] = “Your WiFI Password”;
+
+Servo myservo;
+#define TRIGGERPIN 11
+#define ECHOPIN 12
+#define IR 10
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+ESP8266 wifi( & Serial);
+
+long distance, duration;
+
+void setup() {
+  myservo.attach(9);
+  Serial.begin(ESP8266_BAUD);
+  delay(10);
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(IR, INPUT);
+  Blynk.begin(auth, wifi, ssid, pass);
+}
+
+void sendSensor() {
+  // start working…
+  digitalWrite(TRIGGERPIN, LOW);
+  delayMicroseconds(2);
+  // Sets the TRIGGERPIN on the HIGH state for 10 microseconds
+  digitalWrite(TRIGGERPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGERPIN, LOW);
+  // Reads the ECHOPIN1, returns the sound wave travel time in microseconds
+  duration = pulseIn(ECHOPIN, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print(“Distance: “);
+  Serial.println(distance);
+  Blynk.virtualWrite(V1, distance);
+  delay(500);
+
+  if (digitalRead(IR)) {
+    Serial.println(“enter”);
+    myservo.write(120);
+  } else {
+    myservo.write(0);
   }
+}
 
-  //int LED  = 13;
-  #include <SoftwareSerial.h>
-
-  #define BTTX 9
-  #define BTRX 10
-  SoftwareSerial SerialBT(BTTX, BTRX);
-
-  void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
-    SerialBT.begin(9600);
-    SerialBT.println("Bluetooth connection established");
-  }
-
-  void loop() {
-    delay(300);
-    if (SerialBT.available()) {
-      String msg = SerialBT.readString();
-      if (msg == "on\r\n") {
-        digitalWrite(LED_BUILTIN, HIGH);
-        SerialBT.println("LED is ON");
-      } else
-      if (msg == "off\r\n") {
-        digitalWrite(LED_BUILTIN, LOW);
-        SerialBT.println("LED is OFF");
-      }
-    }
-  }
-
-  98 d3: 31: fda812
-  98 D3, 31, FDA812
+void loop() {
+  sendSensor();
+  Blynk.run();
+}
